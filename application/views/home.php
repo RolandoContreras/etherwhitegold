f84f512ac8433524f5a123dc4a381e9e4818c0b0
software.contreras@gmail.com <software.contreras@gmail.com>
2018-03-19 15:26:17 -0500

    changes pm responsive

--- application/views/home.php
+++ application/views/home.php
@@ -40,7 +40,7 @@
                     <div class="wellcome-heading">
                         <h2><?php echo $round." : ".$percent."% Discount";?></h2>
                         <div class="progress">
-                            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="13000000" style="width: 15%"><?php echo $obj_total_eth;?> ETH</div>
+                            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="13000000" style="width: 20%"><?php echo $obj_total_eth;?> ETH</div>
                         </div>
                     </div>
                     <div class="get-start-area">
